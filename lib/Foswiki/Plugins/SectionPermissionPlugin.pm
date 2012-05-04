dd529a3c332b2d3c8961d392ba163d2384c9eb98
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/SectionPermissionPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/SectionPermissionPlugin.pm
+++ lib/Foswiki/Plugins/SectionPermissionPlugin.pm
@@ -17,27 +17,27 @@ package Foswiki::Plugins::SectionPermissionPlugin;
 
 use strict;
 
-require Foswiki::Func;    # The plugins API
-require Foswiki::Plugins; # For the API version
+require Foswiki::Func;       # The plugins API
+require Foswiki::Plugins;    # For the API version
 
 our $VERSION = '$Rev: 3048 (2009-03-12) $';
 our $RELEASE = 'v1.0';
-our $SHORTDESCRIPTION = 'Deny or allow view of sections for specific wiki groups';
+our $SHORTDESCRIPTION =
+  'Deny or allow view of sections for specific wiki groups';
 our $NO_PREFS_IN_TOPIC = 1;
 
 # global vars
 use vars qw($currentUser);
 
-
 sub initPlugin {
-    my( $topic, $web, $user, $installWeb ) = @_;
+    my ( $topic, $web, $user, $installWeb ) = @_;
 
     $currentUser = $user;
 
     # check for Plugins.pm versions
-    if( $Foswiki::Plugins::VERSION < 2.0 ) {
+    if ( $Foswiki::Plugins::VERSION < 2.0 ) {
         Foswiki::Func::writeWarning( 'Version mismatch between ',
-                                     __PACKAGE__, ' and Plugins.pm' );
+            __PACKAGE__, ' and Plugins.pm' );
         return 0;
     }
 
@@ -47,22 +47,27 @@ sub initPlugin {
 
 sub commonTagsHandler {
 
-  my ( $text, $topic, $web, $included, $meta ) = @_;
-    
-  while($_[0] =~ s/%STARTPERMISSION{(?!.*%STARTPERMISSION)(.*?)}%(.*?)%STOPPERMISSION%/&sectionPermissions($1, $2)/ges){}
-  
+    my ( $text, $topic, $web, $included, $meta ) = @_;
+
+    while ( $_[0] =~
+s/%STARTPERMISSION{(?!.*%STARTPERMISSION)(.*?)}%(.*?)%STOPPERMISSION%/&sectionPermissions($1, $2)/ges
+      )
+    {
+    }
+
 }
 
 sub initCore {
 
-  require Foswiki::Plugins::SectionPermissionPlugin::Core;
-  Foswiki::Plugins::SectionPermissionPlugin::Core::init($currentUser);
+    require Foswiki::Plugins::SectionPermissionPlugin::Core;
+    Foswiki::Plugins::SectionPermissionPlugin::Core::init($currentUser);
 }
 
 sub sectionPermissions {
-  
-  initCore();
-  return Foswiki::Plugins::SectionPermissionPlugin::Core::sectionPermissions(@_);
+
+    initCore();
+    return Foswiki::Plugins::SectionPermissionPlugin::Core::sectionPermissions(
+        @_);
 }
 
 1;
--- lib/Foswiki/Plugins/SectionPermissionPlugin/Core.pm
+++ lib/Foswiki/Plugins/SectionPermissionPlugin/Core.pm
@@ -5,52 +5,54 @@ package Foswiki::Plugins::SectionPermissionPlugin::Core;
 
 use strict;
 
-require Foswiki::Func;    # The plugins API
-require Foswiki::Plugins; # For the API version
+require Foswiki::Func;       # The plugins API
+require Foswiki::Plugins;    # For the API version
 
 use vars qw($currentUser);
 
 sub init {
-  ($currentUser) = @_;
+    ($currentUser) = @_;
 }
 
 sub sectionPermissions {
-  
-  my ($attributes, $theText) = @_;
-  
-  $attributes ||= '';
-  
-  # return if current user is an admin
-  return $theText if Foswiki::Func::isAnAdmin($currentUser);
-      
-  my %params = Foswiki::Func::extractParameters($attributes);
-    
-  my $allow = $params{allow} || $params{"_DEFAULT"} || '';
-  
-  my $deny = $params{deny} || '';
-    
-  # allow a grouplist from view
-  if( ($allow ne '') && ($deny eq '') ) {
-       
-    my $validGroup = 0;
-    foreach my $group ( split(/\s*,\s*/, $allow) ) {
-      
-      $validGroup = 1 if(Foswiki::Func::isGroup($group));
-      return $theText if(Foswiki::Func::isGroupMember( $group, $currentUser ));
+
+    my ( $attributes, $theText ) = @_;
+
+    $attributes ||= '';
+
+    # return if current user is an admin
+    return $theText if Foswiki::Func::isAnAdmin($currentUser);
+
+    my %params = Foswiki::Func::extractParameters($attributes);
+
+    my $allow = $params{allow} || $params{"_DEFAULT"} || '';
+
+    my $deny = $params{deny} || '';
+
+    # allow a grouplist from view
+    if ( ( $allow ne '' ) && ( $deny eq '' ) ) {
+
+        my $validGroup = 0;
+        foreach my $group ( split( /\s*,\s*/, $allow ) ) {
+
+            $validGroup = 1 if ( Foswiki::Func::isGroup($group) );
+            return $theText
+              if ( Foswiki::Func::isGroupMember( $group, $currentUser ) );
+        }
+
+        return '' if ( $validGroup == 1 );
     }
-    
-    return '' if($validGroup == 1);
-  }
-  
-  # deny a grouplist from view
-  elsif( ($deny ne '') && ($allow eq '') ) {
-    
-    foreach my $group ( split(/\s*,\s*/, $deny) ) {
-      return '' if( Foswiki::Func::isGroupMember( $group, $currentUser ));
+
+    # deny a grouplist from view
+    elsif ( ( $deny ne '' ) && ( $allow eq '' ) ) {
+
+        foreach my $group ( split( /\s*,\s*/, $deny ) ) {
+            return ''
+              if ( Foswiki::Func::isGroupMember( $group, $currentUser ) );
+        }
     }
-  }
-      
-  return $theText;
+
+    return $theText;
 }
 
 1;
